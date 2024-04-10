583dbb3dac9cb5b875290608384155d984b9a349
Timo <38291523+lovetodream@users.noreply.github.com>
2024-04-10 14:07:34 +0200

    refactor: downgrade back to swift 5.3

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.10
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "CBORCoding",
-    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10)],
+    platforms: [.iOS(.v12), .macOS(.v11), .tvOS(.v12), .watchOS(.v6)],
     products: [.library(name: "CBORCoding", targets: ["CBORCoding"])],
     dependencies: [],
     targets: [
