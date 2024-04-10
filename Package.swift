f214f73b8b5ee9d18248529ca748a75dbe417e06
Timo <38291523+lovetodream@users.noreply.github.com>
2024-04-10 14:08:55 +0200

    refactor: allow macOS 10.13 again

--- Package.swift
+++ Package.swift
@@ -3,9 +3,18 @@ import PackageDescription
 
 let package = Package(
     name: "CBORCoding",
-    platforms: [.iOS(.v12), .macOS(.v11), .tvOS(.v12), .watchOS(.v6)],
+
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .tvOS(.v12),
+        .watchOS(.v6)
+    ],
+
     products: [.library(name: "CBORCoding", targets: ["CBORCoding"])],
+    
     dependencies: [],
+
     targets: [
         .target(name: "CBORCoding"),
         .testTarget(name: "CBORCodingTests", dependencies: ["CBORCoding"])
